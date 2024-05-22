2600b8c9b9ed9eacef22aa2bfe0641e8d4bdc1e9
AlkyePaul <paolo@viajaconlaia.com>
2024-05-23 00:02:30 +0200

    quasi finito mercoledì
    non aggiunto è il lavoro di ieri ma ho capito come funziona pages

---
+++ assets/customjs/custom.js
@@ -0,0 +1,19 @@
+
+
+
+console.log('I just ran');
+  const myTimeout = setTimeout(showDelayed, 4000);
+
+    function showDelayed() {
+      document.getElementById("typewriter-div-2").style.display = "block"};
+
+  const typewriterOff = setTimeout(typeOff, 4250);
+
+    function typeOff() {
+      document.getElementById("typewriter1").style.border = "none"}
+
+
+	  jQuery(document).ready(function($) 
+    {$(".accordion-button").click(function()
+    { this.scrollIntoView({behavior: 'smooth', block: 'center' });})});
+  
