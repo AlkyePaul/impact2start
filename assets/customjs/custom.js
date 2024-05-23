8736895ac8281bf67158b13dd780aacf46221da0
AlkyePaul <paolo@viajaconlaia.com>
2024-05-24 00:28:13 +0200

    Update custom.js

--- assets/customjs/custom.js
+++ assets/customjs/custom.js
@@ -8,7 +8,6 @@ console.log('I just ran');
   });
 });
 
-window.onload = function() {
   document.getElementById('contact-form').addEventListener('submit', function(event) {
       event.preventDefault();
       // these IDs from the previous steps
@@ -19,7 +18,6 @@ window.onload = function() {
               console.log('FAILED...', error);
           });
   });
-}
 
   const myTimeout = setTimeout(showDelayed, 4000);
 
