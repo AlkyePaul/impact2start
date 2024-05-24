9cc9082fd8141218118662bfbe3a01999d8d0dbf
AlkyePaul <paolo@viajaconlaia.com>
2024-05-24 09:25:31 +0200

    forse oggi finiamo...
    hola

--- assets/customjs/custom.js
+++ assets/customjs/custom.js
@@ -1,23 +1,7 @@
 
 console.log('I just ran');
 
-(function() {
-  // https://dashboard.emailjs.com/admin/account
-  emailjs.init({
-    publicKey: "NoE6Up0hSUYQnokoz",
-  });
-});
 
-  document.getElementById('contact-form').addEventListener('submit', function(event) {
-      event.preventDefault();
-      // these IDs from the previous steps
-      emailjs.sendForm('contact_service', 'contact_form', this)
-          .then(() => {
-              console.log('SUCCESS!');
-          }, (error) => {
-              console.log('FAILED...', error);
-          });
-  });
 
   const myTimeout = setTimeout(showDelayed, 4000);
 
